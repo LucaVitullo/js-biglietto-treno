7e35b493f4a0cdd0f71d6b46dd47e47eaaae9203
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-04 14:56:11 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('js ok')
+
+const Percorso = prompt('Inserisci quanti chilometri vuoi percorrere');
+const EtàPasseggero = prompt('Inserisci letà del passeggero');
+const PrezzoPerKm = 0.21;
+const PrezzoPercorso = PrezzoPerKm * Percorso;
+
+console.log('PrezzoPercorso')
+
+if(EtàPasseggero <= 18){
+
+
+
+}
