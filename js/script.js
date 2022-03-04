59b25f543905c083729e6bde50f3f7de00cabd7e
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-04 15:37:19 +0100

    prove

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@ console.log('js ok')
 const Percorso = prompt('Inserisci quanti chilometri vuoi percorrere');
 const EtàPasseggero = prompt('Inserisci letà del passeggero');
 const PrezzoPerKm = 0.21;
-const PrezzoPercorso = PrezzoPerKm * Percorso;
+const PrezzoBiglietto = PrezzoPerKm * Percorso;
+let PrezzoTotale;
 
-console.log('PrezzoPercorso')
+//sconto 20%
+let ScontoMin = (PrezzoBiglietto * 20 / 100 );
+
+//sconto 40%
+let ScontoOver65 = (PrezzoBiglietto * 40 / 100 );
+
+console.log('PrezzoBiglietto');
 
 if(EtàPasseggero <= 18){
 
+PrezzoTotale = (PrezzoBiglietto - ScontoMin );
 
+document.getElementById ("PrezzoTotale").innerHTML = "Il prezzo finale è: PrezzoTotale"
 
 }
