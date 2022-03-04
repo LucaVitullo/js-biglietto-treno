d364f09e9b3c940703c72431ad1ea523d92a57c6
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-04 16:29:17 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -4,20 +4,34 @@ const Percorso = prompt('Inserisci quanti chilometri vuoi percorrere');
 const EtàPasseggero = prompt('Inserisci letà del passeggero');
 const PrezzoPerKm = 0.21;
 const PrezzoBiglietto = PrezzoPerKm * Percorso;
-let PrezzoTotale;
-
 //sconto 20%
 let ScontoMin = (PrezzoBiglietto * 20 / 100 );
 
 //sconto 40%
 let ScontoOver65 = (PrezzoBiglietto * 40 / 100 );
 
-console.log('PrezzoBiglietto');
+//console.log('PrezzoBiglietto');
 
 if(EtàPasseggero <= 18){
+    console.log('hai uno sconto del 20%')
 
-PrezzoTotale = (PrezzoBiglietto - ScontoMin );
+ let PrezzoTotale = (PrezzoBiglietto - ScontoMin ).toFixed(2);
 
-document.getElementById ("PrezzoTotale").innerHTML = "Il prezzo finale è: PrezzoTotale"
+ document.getElementById("PrezzoTotale").innerHTML = "Il prezzo finale è: "+PrezzoTotale+"€ "
+
+}else if(EtàPasseggero >= 65){
+    console.log('hai uno sconto del 40%')
+
+  let  PrezzoTotale = (PrezzoBiglietto - ScontoOver65 );
+    
+    document.getElementById("PrezzoTotale").innerHTML = "Il prezzo finale è: "+PrezzoTotale+"€ "
+
+
+    
+}else {
+    console.log('il prezzo del biglietto', PrezzoBiglietto + '€' );
+    document.getElementById("PrezzoTotale").innerHTML = "Il prezzo finale è: "+PrezzoBiglietto+"€ "
+
 
 }
+
